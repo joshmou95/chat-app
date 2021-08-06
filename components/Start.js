f6153ed50d6affb871c84d5272808f9948a0dbe7
Joshua Mouton <80426764+joshmou95@users.noreply.github.com>
2021-08-06 17:27:25 -0600

    update styles

--- components/Start.js
+++ components/Start.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, Button, TextInput, StyleSheet, TouchableOpacity, ImageBackground  } from 'react-native';
+import { useState } from 'react/cjs/react.development';
 
 export default class Start extends React.Component {
   constructor(props) {
@@ -10,50 +11,47 @@ export default class Start extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-      <ImageBackground
-        source={require('../assets/Background_Image.png') }
-        resizeMode='cover'
-        style={styles.image}
-        >
-        <Text style={styles.title}>Chat App</Text>
-        <View style={{
-          width: '88%',
-          height: '44%',
-          backgroundColor: '#FFFFFF'
-        }}> 
-          <TextInput
-            style={styles.input}
-            onChangeText={(name) => this.setState({name})}
-            value={this.state.name}
-            placeholder='Your name'
-          />
-          <Text
-            style={{
-              fontSize: 16,
-              fontWeight: '300',
-              color: '#757083',
-              opacity: 1,
-              margin: 15
-            }}
-          >Choose Background Color:</Text>
-          <View>
-            <TouchableOpacity
-              style={styles.button}
-              title="Start Chatting"
-              onPress={() => 
-                this.props.navigation.navigate('Chat', { name: this.state.name })}
-            >
+        <ImageBackground
+          source={require('../assets/Background_Image.png') }
+          resizeMode='cover'
+          style={styles.image}
+          >
+          <Text style={styles.title}>Chat App</Text>
+          <View style={{
+            width: '88%',
+            height: '44%',
+            backgroundColor: '#FFFFFF',
+            }}> 
+            <TextInput
+              style={styles.input}
+              onChangeText={(name) => this.setState({name})}
+              value={this.state.name}
+              placeholder='Your name'
+            />
             <Text
               style={{
                 fontSize: 16,
-                fontWeight: '600',
-                color: '#FFFFFF',
-                alignSelf: 'center',
+                fontWeight: '300',
+                color: '#757083',
+                opacity: 1,
+                margin: 15
               }}
-            >Start Chatting</Text>
-            </TouchableOpacity>
+              >Choose Background Color:</Text>
+              <TouchableOpacity
+                style={styles.button}
+                title="Start Chatting"
+                onPress={() => 
+                  this.props.navigation.navigate('Chat', { name: this.state.name })}
+              >
+              <Text
+                style={{
+                  fontSize: 16,
+                  fontWeight: '600',
+                  color: '#FFFFFF',
+                }}
+              >Start Chatting</Text>
+              </TouchableOpacity>
           </View>
-        </View>
         </ImageBackground>
       </View>
     )
@@ -63,9 +61,9 @@ export default class Start extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
-    height: '100%'
+    // height: '100%'
   },
   title: {
     fontSize: 45,
@@ -79,7 +77,8 @@ const styles = StyleSheet.create({
     color: '#FFFFFF',
     width: '88%',
     height: 50,
-    padding: 10
+    padding: 15,
+    alignItems: 'center'
   },
   image: {
     width: '100%',
@@ -99,3 +98,59 @@ const styles = StyleSheet.create({
     opacity: .5
   },
 });
+
+// const Start = (props) => {
+//   const [ name, setText ] = useState('');
+
+//     return (
+//       <View style={styles.container}>
+//       <ImageBackground
+//         source={require('../assets/Background_Image.png') }
+//         resizeMode='cover'
+//         style={styles.image}
+//         >
+//         <Text style={styles.title}>Chat App</Text>
+//         <View style={{
+//           width: '88%',
+//           height: '44%',
+//           backgroundColor: '#FFFFFF'
+//         }}> 
+//           <TextInput
+//             style={styles.input}
+//             placeholder='Your name'
+//             onChangeText={name => setText(name)}
+//             defaultValue={name}
+//           />
+//           <Text
+//             style={{
+//               fontSize: 16,
+//               fontWeight: '300',
+//               color: '#757083',
+//               opacity: 1,
+//               margin: 15
+//             }}
+//           >Choose Background Color:</Text>
+//           <View>
+//             <TouchableOpacity
+//               style={styles.button}
+//               title="Start Chatting"
+//               onPress={() => 
+//                 this.props.navigation.navigate('Chat', { name: this.state.name })}
+//             >
+//             <Text
+//               style={{
+//                 fontSize: 16,
+//                 fontWeight: '600',
+//                 color: '#FFFFFF',
+//                 alignSelf: 'center',
+//               }}
+//             >Start Chatting</Text>
+//             </TouchableOpacity>
+//           </View>
+//         </View>
+//         </ImageBackground>
+//       </View>
+//     )
+// }
+
+// export default Start;
