df1dc3b1903c256e0b47235871b3fc4fcbbea33d
Joshua Mouton <80426764+joshmou95@users.noreply.github.com>
2021-08-06 16:47:04 -0600

    add comments

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import the screens we want to navigate
 import Start from './components/Start';
 import Chat from './components/Chat';
+import { StyleSheet, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from 'expo-status-bar';
 
 const Stack = createStackNavigator();
 
