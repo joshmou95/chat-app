bb2bdd20be8e5ccb868f10e842ee54e9e2a8dc1d
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-08-08 18:03:41 -0600

    passed backColor state

--- components/Chat.js
+++ components/Chat.js
@@ -2,12 +2,33 @@ import React from 'react'
 import { View, Text } from 'react-native';
 
 export default class Chat extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      name: '',
+      backColor: this.props.route.params.backColor
+    };
+  }
+
+  componentDidMount() {
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name })
+  }
+
+  render() {
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Hello Chat!</Text>
+      <View style={{
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center',
+        backgroundColor: this.state.backColor
+        }}>
+        <Text style={{
+          color: '#FFFFFF',
+          fontSize: 20,
+          fontWeight: '600'
+        }}
+        >Hello Chat!</Text>
       </View>
     )
   }
