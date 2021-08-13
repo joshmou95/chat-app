13103de282bf90c404b4094c6b480572faa3e421
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-08-12 21:12:08 -0600

    Add Bubble and GiftedChat

--- components/Chat.js
+++ components/Chat.js
@@ -1,35 +1,81 @@
 import React from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
 export default class Chat extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
       name: '',
-      backColor: this.props.route.params.backColor
+      backColor: this.props.route.params.backColor,
+      messages: [],
     };
   }
 
   componentDidMount() {
     let name = this.props.route.params.name;
-    this.props.navigation.setOptions({ title: name })
+    this.props.navigation.setOptions({ title: name });
+
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+        {
+          _id: 2, 
+          text: `${this.props.route.params.name} entered the chat`,
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#7A87DB'
+          }
+        }}
+      />
+    )
   }
 
   render() {
     return (
       <View style={{
         flex: 1, 
-        justifyContent: 'center', 
-        alignItems: 'center',
         backgroundColor: this.state.backColor
         }}>
-        <Text style={{
-          color: '#FFFFFF',
-          fontSize: 20,
-          fontWeight: '600'
-        }}
-        >Hello {this.props.route.params.name}!</Text>
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
       </View>
     )
   }
 }
+
+
+
