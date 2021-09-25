16bedad3b5c290dae86153028539d672cfbbab68
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-09-25 13:10:14 -0600

    remove unused

--- components/Chat.js
+++ components/Chat.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Platform, KeyboardAvoidingView, LogBox, Animated } from 'react-native';
+import { View, Platform, KeyboardAvoidingView, LogBox, Animd } from 'react-native';
 import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import NetInfo from '@react-native-community/netinfo';
@@ -246,7 +246,6 @@ export default class Chat extends React.Component {
           renderCustomView={this.renderCustomView}
           onSend={(messages) => this.onSend(messages)}
           user={this.state.user}
-          
         />
         { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
       </View>
